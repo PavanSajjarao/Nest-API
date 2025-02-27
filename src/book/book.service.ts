8fb746a51d045a6b659d28fbe9cf6c75f0baf5fe
PavanSajjarao <pavankrishna.s@exto360.com>
2025-02-27 12:42:56 +0530

    added pagnation settings

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -14,7 +14,7 @@ export class BookService {
     ) {}
 
     async findAll(query: ExpressQuery): Promise<Book[]> {
-        const resPerPage = 2;
+        const resPerPage = 10;
         const currentPage = Number(query.page) || 1;
         const skip = resPerPage * (currentPage - 1);
 
